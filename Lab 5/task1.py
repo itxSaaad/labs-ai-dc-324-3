ca6bd45c19be822705491d6510844cdf83ecba14
Muhammad Saad <saadstudent.cs@gmail.com>
2024-08-05 20:45:22 +0500

    Added: Task 1

--- Lab 5/task1.py
+++ Lab 5/task1.py
@@ -1,27 +1,51 @@
 from collections import deque
 
-def breadth_first_search(graph, start):
-    visited = set()
-    queue = deque([start])
+class Graph:
+    def __init__(self):
+        self.graph = {}
 
-    visited.add(start)
+    def add_edge(self, node, neighbour):
+        if node not in self.graph:
+            self.graph[node] = []
+        if neighbour not in self.graph:
+            self.graph[neighbour] = []
+        self.graph[node].append(neighbour)
 
-    while queue:
-        node = queue.popleft()
-        print(node, end=" ")
+    def breadth_first_search(self, start,goal):
+        visited = set()
+        queue = deque([start])
 
-        for neighbour in graph[node]:
-            if neighbour not in visited:
-                visited.add(neighbour)
-                queue.append(neighbour)
+        visited.add(start)
 
-graph = {
-    'A': ['B', 'C'],
-    'B': ['D', 'E'],
-    'C': ['F'],
-    'D': [],
-    'E': ['F'],
-    'F': []
+        while queue:
+            node = queue.popleft()
+            print(node, end=" ")
+
+            if node == goal:
+                print("\nFound target")
+                return
+
+            for neighbour in self.graph[node]:
+                if neighbour not in visited:
+                    visited.add(neighbour)
+                    queue.append(neighbour)
+
+        print("\nTarget not found")
+        return
+
+
+dataGraph = {
+    '0': {'1', '4'},
+    '1': {'0', '2', '3', '4'},
+    '2': {'1', '3'},
+    '3': {'1', '2', '4'},
+    '4': {'0', '1', '3'}
 }
 
-breadth_first_search(graph, 'A')
+graph = Graph()
+
+for node, neighbours in dataGraph.items():
+    for neighbour in neighbours:
+        graph.add_edge(node, neighbour)
+        
+graph.breadth_first_search('0', '3')
