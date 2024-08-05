875a1eb15a3a6f54bdcae44a1bd2da8476d9d968
Muhammad Saad <saadstudent.cs@gmail.com>
2024-08-05 20:43:39 +0500

    Added: Task 2

--- Lab 5/task2.py
+++ Lab 5/task2.py
@@ -1,35 +1,51 @@
 from collections import deque
 
-def breadth_first_search(graph, start, target):
-    visited = set()
-    queue = deque([start])
+class Graph:
+    def __init__(self):
+        self.graph = {}
 
-    visited.add(start)
+    def add_edge(self, node, neighbour):
+        if node not in self.graph:
+            self.graph[node] = []
+        if neighbour not in self.graph:
+            self.graph[neighbour] = []
+        self.graph[node].append(neighbour)
 
-    while queue:
-        node = queue.popleft()
-        print(node, end=" ")
+    def breadth_first_search(self, start,goal):
+        visited = set()
+        queue = deque([start])
 
-        if node == target:
-            print("\nFound target")
-            return
+        visited.add(start)
 
-        for neighbour in graph[node]:
-            if neighbour not in visited:
-                visited.add(neighbour)
-                queue.append(neighbour)
+        while queue:
+            node = queue.popleft()
+            print(node, end=" ")
 
-    print("\nTarget not found")
-    return
+            if node == goal:
+                print("\nFound target")
+                return
 
-graph = {
-    'A': ['B', 'C', 'D'],
-    'B': [ 'E', 'F'],
-    'C': ['G'],
-    'D': [],
-    'E': [],
-    'F': [],
-    'G': []
+            for neighbour in self.graph[node]:
+                if neighbour not in visited:
+                    visited.add(neighbour)
+                    queue.append(neighbour)
+
+        print("\nTarget not found")
+        return
+
+dataGraph = {
+    'A': {'B', 'C', 'D'},
+    'B': {'E', 'F'},
+    'C': {'G'},
+    'D': {},
+    'E': {},
+    'F': {},
+    'G': {}
 }
 
-breadth_first_search(graph, 'A', 'E')
+graph = Graph()
+for node, neighbours in dataGraph.items():
+    for neighbour in neighbours:
+        graph.add_edge(node, neighbour)
+
+graph.breadth_first_search('A', 'G')
