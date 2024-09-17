7ac185bc08feae9c60fb23beaa121f7425e6ea1c
itxSaaad <saadstudent.cs@gmail.com>
2024-09-17 20:43:30 +0500

    Added: Lab 9

--- Lab 9/task1.py
+++ Lab 9/task1.py
@@ -2,24 +2,24 @@ def minimax(node, depth, isMaximizingPlayer, alpha, beta):
     if depth == 0 or len(node[1]) == 0:
         return node[0]
     if isMaximizingPlayer:
-        bestVal = float('-inf')
+        best_val = float('-inf')
         for child in node[1]:
             value = minimax(child, depth - 1, False, alpha, beta)
-            bestVal = max(bestVal, value)
-            alpha = max(alpha, bestVal)
+            best_val = max(best_val, value)
+            alpha = max(alpha, best_val)
             if beta <= alpha:
                 break
-        return bestVal
+        return best_val
     else:
-        bestVal = float('inf')
+        best_val = float('inf')
         for child in node[1]:
             value = minimax(child, depth - 1, True, alpha, beta)
-            bestVal = min(bestVal, value)
-            beta = min(beta, bestVal)
+            best_val = min(best_val, value)
+            beta = min(beta, best_val)
             if beta <= alpha:
                 break
-        return bestVal
-    
+        return best_val
+
 tree = [3, [[5, [[9, [[8, [], []], [6, [], []]]], [6, [], []]], [2, [[3, [[1, [], []]], [7, [], []]]]]]]]
 
 print(minimax(tree, 3, True, float('-inf'), float('inf')))
--- Lab 9/task2.py
+++ Lab 9/task2.py
@@ -2,18 +2,18 @@ def minimax(node, depth, maximizingPlayer):
     if depth == 0 or len(node[1]) == 0:
         return node[0]
     if maximizingPlayer:
-        maxEva = float('-inf')
+        max_eva = float('-inf')
         for child in node[1]:
             eva = minimax(child, depth - 1, False)
-            maxEva = max(maxEva, eva)   
-        return maxEva
+            max_eva = max(max_eva, eva)
+        return max_eva
     else:
-        minEva = float('inf')
+        min_eva = float('inf')
         for child in node[1]:
             eva = minimax(child, depth - 1, True)
-            minEva = min(minEva, eva)
-        return minEva
-    
+            min_eva = min(min_eva, eva)
+        return min_eva
+
 tree = [3, [[5, [[9, [[8, [], []], [6, [], []]]], [6, [], []]], [2, [[3, [[1, [], []]], [7, [], []]]]]]]]
 
 print(minimax(tree, 3, True))
